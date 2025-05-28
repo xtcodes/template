108ad3c4a26f635cf0303a12a58bf3b1d3cda66e
Ferry Ayunda <ayunda.fa0604@gmail.com>
2025-05-28 10:17:39 +0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,13 +2,24 @@
 	export let name;
 </script>
 
-<main>
-	<div id="loading">Memuat...</div>
+        <div id="loading">Memuat...</div>
         <div class="alert">
         <h1>Selamat {name}!</h1>
         <p style="padding-top:10px;color:#000;">Ini adalah situs web tempat Anda dapat menambahkan foto ke bingkai.</p>
         <a href="#" class="close_btn"><i class="bi bi-x-square-fill"></i></a>
         </div>
+
+<main>
+	<h1>Editor Twibbon</h1>
+        <p><canvas id="canvas" width="1024" height="1024"></canvas></p>
+        <p>
+        <input type="file" id="upload" accept="image/*" />
+        <button class="file-label" id="btnUploadUser"><i class="bi bi-camera"></i></button>
+        <input type="file" id="uploadTwibbon" accept="image/*" />
+        <button class="twibbon-label" onclick="selectTwibbon()"><i class="bi bi-layers"></i></button>
+        <button class="btn" id="download"><i class="bi bi-download"></i></button>
+        <button class="btn" id="share"><i class="bi bi-share"></i> Bagikan</button>
+        </p>
 </main>
 
 <style>
