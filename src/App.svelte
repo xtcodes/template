23e3d4266c352dd7c83ddcfa73eeef0870cfc258
Ferry Ayunda <ayunda.fa0604@gmail.com>
2025-05-28 10:04:23 +0700

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -9,22 +9,15 @@
 
 <style>
 	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
+		width: 600px; 
+                margin: 5em auto; 
+                padding: 2em;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
+	@media (max-width: 700px) {
 		main {
-			max-width: none;
+			margin: auto;
+                        width: auto;
 		}
 	}
 </style>
