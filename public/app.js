5d97a1d7756ea6c33b23a404490af415f4e2a60f
Ferry Ayunda <ayunda.fa0604@gmail.com>
2025-05-28 10:41:12 +0700

    Update app.js

--- public/app.js
+++ public/app.js
@@ -139,6 +139,6 @@ alert("Perangkat Anda tidak mendukung fitur bagikan file.");
 
 if ('serviceWorker' in navigator) {
 window.addEventListener('load', () => {
-navigator.serviceWorker.register('sw.js');
+navigator.serviceWorker.register('/sw.js');
 });
 }
